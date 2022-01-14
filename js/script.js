17e610010f17de5f379ed08707ce4edf081d9c3d
enricopic85 <enricodev85@gmail.com>
2022-01-14 15:24:18 +0100

    primo push

---
+++ js/script.js
@@ -0,0 +1,5 @@
+new Vue({
+    el:"#app",
+    img:['https://www.reterurale.it/flex/images/c/e/2/D.3a4854418244e96b18c8/1.jpg','https://www.reterurale.it/flex/images/9/2/3/D.351363a0300d2b7790bd/1.jpg','https://siviaggia.it/wp-content/uploads/sites/2/2021/09/Paesaggi-mondo.jpg','https://siviaggia.it/wp-content/uploads/sites/2/2021/02/paesaggi-primavera.jpg','https://images.everyeye.it/img-notizie/40-ore-video-paesaggi-mozzafiato-pianeta-v3-299389-1280x960.jpg'],
+    currentIndex:0
+})
